50ea08d9a3b1e18aa4a82ca94f5f752cc44d2322
Perodeau <perod.matheo@gmail.com>
2025-04-06 15:54:52 +0200

    Refonte de la structure du code
    Signed-off-by: Perodeau <perod.matheo@gmail.com>

--- src/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TicketSupport.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
